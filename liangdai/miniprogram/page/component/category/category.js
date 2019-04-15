3d0f9178dfa443410c6ce015dd02cf7725fa3b4e
zhouWM <zhouwm@js.com>
2019-04-15 18:02:30 +0800

    完善分类页面

--- liangdai/miniprogram/page/component/category/category.js
+++ liangdai/miniprogram/page/component/category/category.js
@@ -2,31 +2,45 @@ const app = getApp()
 Page({
     data: {
         category: [
-            {name:'果味',id:'guowei'},
-            {name:'蔬菜',id:'shucai'},
-            {name:'炒货',id:'chaohuo'},
-            {name:'点心',id:'dianxin'},
-            {name:'粗茶',id:'cucha'},
-            {name:'淡饭',id:'danfan'}
+            {name: '果味', id: 'guowei'},
+            {name: '蔬菜', id: 'shucai'},
+            {name: '炒货', id: 'chaohuo'},
+            {name: '点心', id: 'dianxin'},
+            {name: '粗茶', id: 'cucha'},
+            {name: '淡饭', id: 'danfan'}
         ],
-        productList:[],
+        productList: [],
         curIndex: 0,
+        categoryName: "",
         isScroll: false,
         toView: 'guowei'
     },
-    onReady(){
+    onReady() {
         var self = this;
 
         self.setData({
-            category:app.globalData.pType
+            category: app.globalData.pType,
+            categoryName: this.data.category[0].name
         })
+        self.onQueryList(this.data.category[0].name, self)
+        /*wx.request({
+            url:'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+            success(res){
+                self.setData({
+                    detail : res.data
+                })
+            }
+        });*/
+
+    },
+    onQueryList: function (type, _this) {
         const db = wx.cloud.database()
         // 查询当前用户所有的 counters
         db.collection('product').where({
-            type: "乳液"
+            type: type
         }).get({
             success: res => {
-                this.setData({
+                _this.setData({
                     // productList: JSON.stringify(res.data, null, 2)
                     productList: res.data
                 })
@@ -40,33 +54,27 @@ Page({
                 console.error('[数据库] [查询记录] 失败：', err)
             }
         })
-        /*wx.request({
-            url:'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
-            success(res){
-                self.setData({
-                    detail : res.data
-                })
-            }
-        });*/
-        
     },
-    switchTab(e){
-      const self = this;
-      this.setData({
-        isScroll: true
-      })
-      setTimeout(function(){
-        self.setData({
-          toView: e.target.dataset.id,
-          curIndex: e.target.dataset.index
+    switchTab(e) {
+        const self = this;
+        this.setData({
+            isScroll: true
         })
-      },0)
-      setTimeout(function () {
-        self.setData({
-          isScroll: false
-        })
-      },1)
-        
+        const categoryName = self.data.category[e.target.dataset.index].name
+        setTimeout(function () {
+            self.onQueryList(categoryName, self)
+            self.setData({
+                toView: e.target.dataset.id,
+                curIndex: e.target.dataset.index,
+                categoryName: categoryName
+            })
+        }, 0)
+        setTimeout(function () {
+            self.setData({
+                isScroll: false
+            })
+        }, 1)
+
     }
-    
+
 })
