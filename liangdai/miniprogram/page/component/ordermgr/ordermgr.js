5396098c1efc89b316dbbfc500fd5fe85418a046
zhouWM <zhouwm@js.com>
2019-04-15 18:42:40 +0800

    添加订单管理页面

---
+++ liangdai/miniprogram/page/component/ordermgr/ordermgr.js
@@ -0,0 +1,37 @@
+// miniprogram/page/component/ordermgr/ordermgr.js
+const app = getApp()
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        orders: []
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        const self = this
+        const db = wx.cloud.database()
+        const data = db.collection('order').where({
+            _openid: app.globalData.openid
+        }).orderBy('createTime', 'desc').get({
+            success: res => {
+                self.setData({
+                    orders: res.data
+                })
+                console.log('[数据库] [查询记录] 成功: ', res)
+            },
+            fail: err => {
+                wx.showToast({
+                    icon: 'none',
+                    title: '查询记录失败'
+                })
+                console.error('[数据库] [查询记录] 失败：', err)
+            }
+        });
+    },
+
+})
